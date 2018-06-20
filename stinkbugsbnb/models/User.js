90cd4daab00594f9d59dd54dd97b64e0f2f0aaf6
Nick <nicholasjohnrichardson@gmail.com>
2018-06-20 13:28:23 +0100

    made user schema

---
+++ stinkbugsbnb/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+require('mongoose-type-email');
+
+
+const UserSchema = new Schema({
+    user_name: {
+    	type: String,
+    	required: true,
+    	minlength: 1,
+    	maxlength: 50
+    	},
+
+    email_address: {
+    	type: mongoose.SchemaTypes.Email,
+    	required: true
+    }
+
+    password: {
+      type: String,
+      required: true,
+      minlength: 8
+    }
+
+  });
+
+module.exports = mongoose.model('User', UserSchema);
