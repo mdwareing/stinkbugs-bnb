b065240b27bb337ccacc3cc247ad5ee8dc7f49ed
Robert Faldo <robertfaldo@gmail.com>
2018-06-21 17:57:41 +0100

    add test for invalid login details

--- stinkbugsbnb/cypress/integration/features/user_logs_in_spec.js
+++ stinkbugsbnb/cypress/integration/features/user_logs_in_spec.js
@@ -4,7 +4,16 @@
 
 context('user can sign in', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000/login')
+    cy.visit('http://localhost:3000/signup')
+    cy.get('input[name=user_name]')
+      .type('TestingTheLoginPage')
+    cy.get('input[name=email_address]')
+      .type('testingtheloginpage@gmail.com')
+    cy.get('input[name=password]')
+      .type('password')
+    cy.get('input[name=password_confirmation]')
+      .type('password')
+    cy.get('#sign_up_form').submit()
   })
 
   // it('requires email', function (){
@@ -26,13 +35,29 @@ context('user can sign in', () => {
   //   cy.contains('email and password combination incorrect')
   // });
 
-  it('Greets user with name on /display-property', function (){
-    cy.get('input[name=email_address]')
-      .type('testforlogin@email.com')
-    cy.get('input[name=password]')
-      .type('password{enter}')
+  context('When login details valid', function(){
+    it('Greets user with name on /display-property', function (){
+      cy.visit('http://localhost:3000/login')
+      cy.get('input[name=email_address]')
+        .type('testingtheloginpage@gmail.com')
+      cy.get('input[name=password]')
+        .type('password{enter}')
 
-    cy.url().should('eq', 'http://localhost:3000/display-property')
-    cy.contains('Welcome, TestForLogin')
+      cy.url().should('eq', 'http://localhost:3000/display-property')
+      cy.contains('Welcome, TestingTheLoginPage')
+    });
   });
+
+  context('When login details invalid', function(){
+    it('Greets user with name on /display-property', function (){
+      cy.visit('http://localhost:3000/login')
+      cy.get('input[name=email_address]')
+        .type('testingtheloginpage@gmail.com')
+      cy.get('input[name=password]')
+        .type('wrongpassword{enter}')
+
+      cy.url().should('eq', 'http://localhost:3000/login')
+    });
+  });
+
 })
