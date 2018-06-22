0a4d3172fb03a0f3bfe4686f25773a76cc4a8f0c
Matthew <mdwareing@gmail.com>
2018-06-22 15:17:49 +0100

    Added email sending via sendgrid

--- stinkbugsbnb/routes/property.js
+++ stinkbugsbnb/routes/property.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const mongoose = require('mongoose');
 const nodemailer = require('nodemailer');
+const sgTransport = require('nodemailer-sendgrid-transport');
 
 var mongoDB = 'mongodb://admin123:admin123@ds161700.mlab.com:61700/stinkbugs-bnb';
 mongoose.connect(mongoDB);
@@ -15,54 +16,50 @@ const Property = mongoose.model('Property');
 
 
 router.get('/:id', function (req, res, next) {
-  Property.findOne ({
-    '_id':req.params.id},
-    function(error,property) {
-    if(!property) {
-      return res.redirect('/display-property');
-   }
-   res.render('property', {
-     property: property
-   });
- });
+  Property.findOne({
+      '_id': req.params.id
+    },
+    function (error, property) {
+      if (!property) {
+        return res.redirect('/display-property');
+      }
+      res.render('property', {
+        property: property
+      });
+    });
 });
 
+
+
 router.post('/sendemail', function (req, res, next) {
-  nodemailer.createTestAccount((err, account) => {
-    const transporter = nodemailer.createTransport({
-       host: 'smtp.ethereal.email',
-       port: 587,
-       auth: {
-           user: 'ugk5sxyxzpyuj3db@ethereal.email',
-           pass: 'eNgGXUpEdK2H7fXsCF'
-       }
-    });
-    // console.log(req.body.guests)
-    // const host_email = Property.findOne ({
-    //   '_id': req.params.id},
-    //   function(error, property) {
-    //     if(!property) {
-    //       return res.redirect('/${req.params.id}')
-    //     }
-    //     return property.email_address;
-    // });
-    let mailOptions = {
-      from: '"Villa Renter" <ugk5sxyxzpyuj3db@ethereal.email>',
-      to: req.body.test,
-      subject: 'Rent boy',
-      text: 'Give me the villa and im bringing the dog',
-      html: '<br>yeah boiiiiiii</br>'
-    };
 
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        return console.log(error);
-      }
-      console.log('Message sent: %s', info.messageId);
-      console.log('Preview URL: %s', nodemailer.getTestMessagerUrl(info));
-    });
+  var options = {
+    auth: {
+      api_key: process.env.SENDGRID_API_KEY
+    }
+  }
+  var mailer = nodemailer.createTransport(sgTransport(options));
+
+  let mailOptions = {
+    from: '"Villa Renter" <stinbug@gmail.com>',
+    to: 'mdwareing@gmail.com',
+    subject: `Booking request for ${req.body.property_name}`,
+    text: `Lead guests name:
+            Group Details: ${req.body.lead_guest_name}
+            Number of adults: ${req.body.guests[0]}
+            Numbers of children: ${req.body.guests[1]}
+            Date of arrival: ${req.body.date_available}
+            Date of departure: ${req.body.available_until}
+            Contact email: ${req.body.email_address}
+            `
+  };
+  
+  mailer.sendMail(mailOptions, function (err, res) {
+    if (err) {
+      console.log(err)
+    }
+    console.log(res);
   });
-
   res.redirect('/display-property');
 })
 
